7d4255bf2b68135127561cba99218b3858456110
Akira Hayakawa <ruby.wktk@gmail.com>
2021-05-02 16:45:54 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/stage2.py
+++ src/stage2.py
@@ -3,13 +3,9 @@ import MeCab
 import numpy as np
 import json
 
+
 os.environ["MECABRC"] = "/etc/mecabrc"
 
-os.makedirs("/out/stage2", exist_ok=True)
-
-ii = []
-for i in os.listdir("/out/stage1"):
-    ii.append(int(i))
 
 # print(ii)
 
@@ -39,7 +35,7 @@ def wakachi(text):
 
 def vecs_array(documents):
     from sklearn.feature_extraction.text import TfidfVectorizer
- 
+
     n = len(documents)
     docs = np.array(documents)
     vectorizer = TfidfVectorizer(analyzer=wakachi, max_df=0.8)
@@ -57,14 +53,22 @@ def vecs_array(documents):
 
 if __name__ == '__main__':
     from sklearn.metrics.pairwise import cosine_similarity
-    docs = []
+
+    os.makedirs("/out/stage2", exist_ok=True)
+
+    ii = []
+    for i in os.listdir("/out/stage1"):
+        ii.append(int(i))
+
+    docs = ["" for i in range(len(ii))]
+    # docs = []
     for i in ii:
         inp = f"/out/stage1/{i}"
         f = open(inp, "r")
         data = f.read()
+        docs[i] = data
         f.close()
         # print(wakachi(data))
-        docs.append(data)
 
     #類似度行列作成
     #小数点3桁まで計算
